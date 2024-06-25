61937b1ffe099d7cb92e1653c8930e830619c4aa
Virtual Sense <virtual_sense@naver.com>
2024-06-25 17:23:45 +0900

    Add files via upload

---
+++ TriangleArea.cs
@@ -0,0 +1,19 @@
+
+namespace ConsoleApp5
+{
+    internal class TriangleArea
+    {
+        static void Main(string[] args)
+        {
+            //1. 변수선언 및 입력부
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            
+            //2. 알고리즘 수식
+            double result = (width * height) / 2.0; //수식
+            
+            //3. 출력부
+            Console.WriteLine($"넓이는 {result} 입니다.");
+        }
+    }
+}
